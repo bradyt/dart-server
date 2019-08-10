4db90aebbbe39d126f6bbb5ae9f694dacbde74e2
Brady Trainor <mail@bradyt.net>
2019-08-10 00:20:49 -0700

    Link license as https

--- dart-server.el
+++ dart-server.el
@@ -21,7 +21,7 @@
 ;; GNU General Public License for more details.
 ;;
 ;; You should have received a copy of the GNU General Public License
-;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 ;; This file contains several functions and variables adapted from the
 ;; code in https://github.com/dominikh/go-mode.el
