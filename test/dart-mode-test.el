df9a8a9c33dd5ecff12d074e1539b64b766ec0c4
Brady Trainor <mail@bradyt.net>
2019-08-25 16:23:00 -0700

    Update ert test with dart-server name

--- test/dart-mode-test.el
+++ test/dart-mode-test.el
@@ -1,12 +1,12 @@
 ;;; dart-mode-test.el --- Tests for dart-mode
 
-(require 'dart-mode)
+(require 'dart-server)
 
 (ert-deftest dart-test ()
   (let ((file (make-temp-file "analysis")))
-    (dart-add-analysis-root-for-file file)
-    (let ((proc (dart--analysis-server-process
-		 dart--analysis-server)))
+    (dart-server-add-analysis-root-for-file file)
+    (let ((proc (dart-server--analysis-server-process
+		 dart-server--analysis-server)))
       (accept-process-output proc)
       (accept-process-output proc)
       (accept-process-output proc 8))))
